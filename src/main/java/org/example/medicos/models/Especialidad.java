df71f86a25aa1d14e047bf8db294e00d47a287a2
Jorgitolindo <152011442+Jorgitolindo@users.noreply.github.com>
2024-11-11 16:29:02 -0400

    rr

--- src/main/java/org/example/medicos/models/Especialidad.java
+++ src/main/java/org/example/medicos/models/Especialidad.java
@@ -18,27 +18,21 @@ public class Especialidad {
     @NotNull(message = "La description no puede ser nulo")
     @Column(nullable = false)
     private String description;
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
